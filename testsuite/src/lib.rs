0ed3e5188fe6cbb5a1901eaecbe9471cfea830fe
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-04-11 11:33:07 +0200

    Also build cargo-minimize

--- testsuite/src/lib.rs
+++ testsuite/src/lib.rs
@@ -63,6 +63,8 @@ pub fn full_tests() -> Result<()> {
         "testsuite",
         "--bin",
         "regression_checker",
+        "--bin",
+        "cargo-minimize",
     ]))
     .context("running cargo build")?;
 
