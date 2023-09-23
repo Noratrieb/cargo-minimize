699a3e1f3d67003cf30b9bf736e8cc9300aebd45
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-09-23 13:29:52 +0200

    update

--- flake.nix
+++ flake.nix
@@ -12,7 +12,12 @@
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           gcc
+          rustup
         ];
+        shellHook = ''
+          export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+          export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+        '';
       };
     });
 }
