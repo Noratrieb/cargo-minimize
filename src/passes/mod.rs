3de6992d63a95250b8615d4a105f731e65243a3d
moxian <moxian@users.noreply.github.com>
2025-03-30 18:32:52 -0700

    Add a split-use pass.
    It transforms "use std::io::{Read, Write}" into
    "use std::io::Read; use std::io::Write"
    
    Which later allows to remove just precisely the statements
    we do not need, and leave rest be.
    
    The test from the last commit does not pass, but that
    is seemingly to do with the test harness setup, since it
    works fine locally.

--- src/passes/mod.rs
+++ src/passes/mod.rs
@@ -2,8 +2,9 @@ mod everybody_loops;
 mod field_deleter;
 mod item_deleter;
 mod privatize;
+mod split_use;
 
 pub use self::{
     everybody_loops::EverybodyLoops, field_deleter::FieldDeleter, item_deleter::ItemDeleter,
-    privatize::Privatize,
+    privatize::Privatize, split_use::SplitUse,
 };
